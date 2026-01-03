ea01adfaff68c3131977cb23d499de086224c9d1
Metin530 <melikzademetin4@gmail.com>
2026-01-03 22:30:19 +0400

    İlk commit 6/1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace almaarmudgoz.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+        public IActionResult SinglePage()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ShopController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace almaarmudgoz.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Wishlist()
+        {
+            return View();
+        }
+    }
+}
